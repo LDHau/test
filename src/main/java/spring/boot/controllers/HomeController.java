68f64c92dcd879ff0a467f99e6dc90c4e2d5853e
DESKTOP-UUAQ27F\Hau <dienhau17@gmail.com>
2021-04-17 19:09:13 +0700

    First Commit

---
+++ src/main/java/spring/boot/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package spring.boot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
